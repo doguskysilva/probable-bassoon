496f3eb0160d8f3abfac9a180f2c0213c93f3907
Douglas Silva <dogusky@protonmail.com>
2024-10-24 00:47:14 -0300

    refactor: lint
    Signed-off-by: Douglas Silva <dogusky@protonmail.com>

--- 3month-preparation-kit/week1/test_plus_minus.py
+++ 3month-preparation-kit/week1/test_plus_minus.py
@@ -1,5 +1,6 @@
 import pytest
 
+
 def plus_minus(arr):
     less_zero = greather_zero = equal_zero = 0
     size_arr = len(arr)
@@ -12,17 +13,19 @@ def plus_minus(arr):
         else:
             equal_zero += 1
 
-    return [round(greather_zero / size_arr, 6), 
-            round(less_zero / size_arr, 6), 
-            round(equal_zero / size_arr, 6)]
+    return [
+        round(greather_zero / size_arr, 6),
+        round(less_zero / size_arr, 6),
+        round(equal_zero / size_arr, 6),
+    ]
+
 
 @pytest.mark.parametrize(
     "test_input,test_expected",
     [
         ([1, 1, 0, -1, -1], [0.400000, 0.400000, 0.200000]),
-        ([-4, 3, -9, 0, 4, 1], [0.500000, 0.333333, 0.166667])
-    ]
+        ([-4, 3, -9, 0, 4, 1], [0.500000, 0.333333, 0.166667]),
+    ],
 )
 def test_plus_minus(test_input, test_expected):
     assert plus_minus(test_input) == test_expected
-
