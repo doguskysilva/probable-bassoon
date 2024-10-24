609557c2f1b4469f177188c6f123cc5c1c2cd0dd
Douglas Silva <dogusky@protonmail.com>
2024-10-23 23:21:01 -0300

    feat: add pythonic soluction to sparse arrays

--- 3month-preparation-kit/week1/test_sparse_arrays.py
+++ 3month-preparation-kit/week1/test_sparse_arrays.py
@@ -14,6 +14,10 @@ def sparse_arrays(strings: List[str], queries: List[str]) -> List[int]:
     return count_queries
 
 
+def sparse_arrays_pythonic(strings: List[str], queries: List[str]) -> List[int]:
+    return [strings.count(query) for query in queries]
+
+
 @pytest.mark.parametrize(
     "test_input_a,test_input_b,test_expected",
     [
@@ -22,3 +26,4 @@ def sparse_arrays(strings: List[str], queries: List[str]) -> List[int]:
 )
 def test_sparse_arrays(test_input_a, test_input_b, test_expected):
     assert sparse_arrays(test_input_a, test_input_b) == test_expected
+    assert sparse_arrays_pythonic(test_input_a, test_input_b) == test_expected
